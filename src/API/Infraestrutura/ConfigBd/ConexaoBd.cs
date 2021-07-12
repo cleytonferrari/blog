3f196a984a21d373482fdf251d3a7068f35e739e
cleytonferrari <cleyton@w7br.com>
2021-07-12 19:19:54 -0400

    Refatorando... para adicionar padrão de repositorio

---
+++ src/API/Infraestrutura/ConfigBd/ConexaoBd.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Infraestrutura.ConfigBd
+{
+    public class ConexaoBd
+    {
+        public static IConfiguration ConfiguracaoDaConexao
+        {
+            get
+            {
+                IConfiguration Configuracao = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+                return Configuracao;
+            }
+        }
+    }
+}
